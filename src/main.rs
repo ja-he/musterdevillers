2996418923dcd1702d5e596b62b4c76955ae39b7
Jan Hensel <ja_he@uni-bremen.de>
2022-01-21 22:46:05 +0100

    Write read data to screen

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let mut stream = stream.unwrap();
 
         println!("connection established");
+
+        let mut buf: [u8; 512] = [0; 512];
+        while let Ok(bytes_read) = stream.read(&mut buf) {
+            if bytes_read <= 0 {
+                break;
+            }
+            print!("{}", String::from_utf8_lossy(&buf[..]));
+        }
     }
 }
