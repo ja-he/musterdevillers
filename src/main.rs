cd3d0352226182b1cdc0930415f1736da28bdd12
Jan Hensel <ja_he@uni-bremen.de>
2022-01-22 15:32:46 +0100

    Sketch out HTTP request types and parse GET
    - also sketches out rough idea of code structure
    - ignores any non-GET
    - drops any headers
    - still only sends default response

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::net::{TcpListener, TcpStream};
 
 use colored::*;
 
+fn print_warning(msg: &str) {
+    println!("{}", msg.red());
+}
+
 fn print_info(msg: &str) {
     println!("{}", msg.blue());
 }
@@ -30,35 +34,65 @@ fn send_default_response(mut stream: TcpStream) {
     stream.flush().unwrap();
 }
 
-fn get_linewise_til_crlfcrlf(stream: &TcpStream) -> Vec<String> {
-    let mut lines = Vec::new();
+#[allow(dead_code)]
+struct HttpGetRequest {
+    request_uri: String,
+    http_version: String,
+    headers: Vec<String>,
+}
+
+#[allow(dead_code)]
+enum HttpRequest {
+    Get(HttpGetRequest),
+    Head,
+    Post,
+    Put,
+    Delete,
+    Connect,
+    Options,
+    Trace,
+    Patch,
+}
+
+fn read_request_from(stream: &TcpStream) -> Option<HttpRequest> {
     let mut reader = BufReader::new(stream.try_clone().unwrap());
-
     let mut line = String::new();
-    while reader.read_line(&mut line).unwrap() > 0 {
-        line = line.trim().to_string();
-        if line.is_empty() {
-            break;
-        }
-        lines.push(line.clone());
-        line = String::from("")
-    }
-
-    return lines;
+    return match reader.read_line(&mut line) {
+        Ok(0) => None,
+        Ok(_) => Some({
+            let tokens: Vec<&str> = line.split_whitespace().collect();
+            match tokens[0] {
+                "GET" => {
+                    assert!(tokens.len() == 3);
+                    let request_uri = tokens[1].to_string();
+                    let http_version = tokens[2].to_string();
+                    let headers: Vec<String> = Vec::new();
+                    HttpRequest::Get(HttpGetRequest {
+                        request_uri,
+                        http_version,
+                        headers,
+                    })
+                }
+                _ => panic!("unhandlable request type"),
+            }
+        }),
+        _ => None,
+    };
 }
 
 fn handle_connection(stream: TcpStream) {
     print_info("connection established");
 
-    for line in get_linewise_til_crlfcrlf(&stream).iter() {
-        println!("{}", line);
+    let request = read_request_from(&stream);
+    match request {
+        Some(HttpRequest::Get(_get_request)) => {
+            print_info("received get request");
+            send_default_response(stream);
+            print_info("connection closed");
+        }
+        Some(_) => print_warning("unhandlable request type"),
+        None => print_warning("no request"),
     }
-
-    print_info("responding...");
-
-    send_default_response(stream);
-
-    print_info("connection closed");
 }
 
 fn main() {
