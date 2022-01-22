a2fd1fa81b0e771fe1ce2f27d872fb60715fc0e3
Jan Hensel <ja_he@uni-bremen.de>
2022-01-22 12:01:31 +0100

    Move connection handling to function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::prelude::*;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 use colored::*;
 
@@ -7,22 +7,25 @@ fn print_info(msg: &str) {
     println!("{}", msg.blue());
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    print_info("connection established");
+    let mut buf: [u8; 512] = [0; 512];
+    while let Ok(bytes_read) = stream.read(&mut buf) {
+        if bytes_read <= 0 {
+            break;
+        }
+        print!("{}", String::from_utf8_lossy(&buf[..]));
+    }
+    println!();
+    print_info("connection closed");
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        print_info("connection established");
-
-        let mut buf: [u8; 512] = [0; 512];
-        while let Ok(bytes_read) = stream.read(&mut buf) {
-            if bytes_read <= 0 {
-                break;
-            }
-            print!("{}", String::from_utf8_lossy(&buf[..]));
-        }
-        println!();
-        print_info("connection closed");
+        handle_connection(stream);
     }
 }
