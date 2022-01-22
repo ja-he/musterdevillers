44792eb4ebf4e94ce6d16a00748120802ee5346f
Jan Hensel <ja_he@uni-bremen.de>
2022-01-22 13:22:47 +0100

    Send HTTP response with default HTML contents
    - no longer reading from stream til closed, but 4096B hard limit
    - next step is validating the request, in which we can find an end
      criteria at which to stop reading

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,39 @@ fn print_info(msg: &str) {
     println!("{}", msg.blue());
 }
 
+fn send_default_response(mut stream: TcpStream) {
+    let default_content = r#"<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Musterdevillers</title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <p>Hi from Musterdevillers!</p>
+  </body>
+</html>"#;
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        default_content.len(),
+        default_content
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 fn handle_connection(mut stream: TcpStream) {
     print_info("connection established");
-    let mut buf: [u8; 512] = [0; 512];
-    while let Ok(bytes_read) = stream.read(&mut buf) {
-        if bytes_read <= 0 {
-            break;
-        }
-        print!("{}", String::from_utf8_lossy(&buf[..]));
-        buf = [0; 512]; // zero out buffer
-    }
+    let mut buf: [u8; 4096] = [0; 4096];
+    stream.read(&mut buf).unwrap();
+    print!("{}", String::from_utf8_lossy(&buf[..]));
     println!();
+
+    print_info("responding...");
+
+    send_default_response(stream);
+
     print_info("connection closed");
 }
 
