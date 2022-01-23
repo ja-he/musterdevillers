f5b3feedef9ecd74ce6c3f57ed0b4d65ee0acacd
Jan Hensel <ja_he@uni-bremen.de>
2022-01-23 18:39:45 +0100

    Add `new()` fn that spawns the threads

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,14 @@ pub struct ThreadPuddle {
 }
 
 impl ThreadPuddle {
+    pub fn new(n: usize) -> ThreadPuddle {
+        assert!(n > 0);
+
+        ThreadPuddle {
+            threads: (0..n).map(|_| {thread::spawn(||{})}).collect(),
+        }
+    }
+
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
